a9d357425cff28eeacf6e6e1756a750145a5ad92
kiranmorrison <kiranmorrison@kubrickgroup.com>
2019-01-08 09:43:19 +0000

    This is the first lot of work

---
+++ learningpythonfunctions.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+'''My Module'''
+
+
+import numpy as np
+import pandas as pd
+import json
+
+
+def main():
+	'''
+	purpose: I am learning variables, this is my main function
+	author: Kiran
+	date: 08/01/2019
+	--
+	ammended on: some day
+	ammended by: itsyaboy
+	issue ticket number: 53035
+	'''
+	myname = 'Kiran'
+	print(myname)
+	
+# execute my main function
+main()
